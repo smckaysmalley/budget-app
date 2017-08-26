febfb7cae8ff5348b2a8b6a029905e44cd1a4b17
McKay Smalley <smckaysmalley@gmail.com>
2017-08-26 10:21:59 -0600

    Reset applicaiton.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151201050840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "allowances", force: :cascade do |t|
     t.text     "owner"
     t.float    "amount"
